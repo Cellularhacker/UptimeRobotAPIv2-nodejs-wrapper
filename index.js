f8ee42b5764db983e4f284f6593333ccd305b8f3
Jeffrey Lee <jeffrey@wizpace.com>
2019-02-03 22:13:45 +0900

    typed all the funcs on APIv2

---
+++ index.js
@@ -0,0 +1,61 @@
+const request = require('request');
+
+function getAccountDetails() {
+
+}
+function getMonitors() {
+
+}
+function newMonitor() {
+    
+}
+function editMonitor() {
+
+}
+function deleteMonitor() {
+
+}
+function resetMonitor() {
+    
+}
+function getAlertContacts() {
+
+}
+function newAlertContact() {
+
+}
+function editAlertContact() {
+
+}
+function deleteAlertContact() {
+
+}
+function getMWindows() {
+
+}
+function newMWindow() {
+
+}
+function editMWindow() {
+
+}
+function deleteMWindow() {
+
+}
+function getPSPs() {
+
+}
+function newPSP() {
+
+}
+function editPSP() {
+    
+}
+function deletePSP() {
+
+}
+
+
+module.exports = {
+
+}
